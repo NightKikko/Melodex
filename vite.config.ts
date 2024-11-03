f2e5aecc66728c367937737445ef59e6e998ce96
NightKikko <126341845+NightKikko@users.noreply.github.com>
2024-11-03 17:30:38 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
-export default {
   build: {
     rollupOptions: {
-      external: ['/src/main.tsx'] // Add the path to the external module
-    }
-  }
-}
+      external: ['/src/main.tsx'], // Add the path to the external module
+    },
+  },
+});
